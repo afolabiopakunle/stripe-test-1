c4287642ccde9331ee6e434ddf5ce8732ddc2746
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-12-31 13:11:49 +0100

    reverse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StripeModule } from "stripe-angular"
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +15,8 @@ import { ArtCardComponent } from './art-card/art-card.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    NgMatsModule
+    NgMatsModule,
+    StripeModule.forRoot('sk_test_51KCY9mE56BwLuZepuJHgpUhzXAwzUBQp2eRoJcZI8NllZee7mf7VBh8ouoWxnFfb1XOCX2D11iy1ceUHNgvgFD0E00gEuLncKx')
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/art-card/art-card.component.ts
+++ src/app/art-card/art-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StripeScriptTag } from "stripe-angular"
+
 
 @Component({
   selector: 'app-art-card',
@@ -7,38 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtCardComponent implements OnInit {
 
-  constructor() { }
+  paymentHandler: any = null
 
-  ngOnInit(): void {
-    this.invokeStripe();
-  }
-
-  makePayment(amount: any) {
-    const paymentHandler = (<any>window).StripeCheckout.configure({
-      key: 'pk_test_51KCY9mE56BwLuZepj0kzE3n8BLfLCfKhcav75HeIIpISFKe2xl1XCgSatNvKXRIogrhT0WMbnBg7nakDynqLOFSU00QcLgdt28',
-      locale: 'auto',
-      token: function(stripeToken: any) {
-        console.log(stripeToken.card);
-        alert('Stripe token generated!');
-      },
-    });
-
-    paymentHandler.open({
-      name: 'Artwork Africa',
-      description: 'Just checking',
-      amount: amount * 100,
-    })
-
-
-  }
-
-  invokeStripe() {
-    if(!window.document.getElementById('stripe-script')) {
-      const script = window.document.createElement('script');
-      script.id = 'stripe.script';
-      script.type = 'text/javascript';
-      script.src = '';
-      window.document.body.appendChild(script);
+  constructor(private stripeScriptTag: StripeScriptTag) {
+    if (!this.stripeScriptTag.StripeInstance) {
+      this.stripeScriptTag.setPublishableKey('sk_test_51KCY9mE56BwLuZepuJHgpUhzXAwzUBQp2eRoJcZI8NllZee7mf7VBh8ouoWxnFfb1XOCX2D11iy1ceUHNgvgFD0E00gEuLncKx');
     }
   }
+
+  ngOnInit(): void {
+
+  }
+
+
+
 }
