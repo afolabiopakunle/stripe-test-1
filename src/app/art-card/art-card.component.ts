f3887b38b8e8eddf08b6495a1982774fb6ce15cb
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-12-31 11:11:43 +0100

    test key added

--- src/app/art-card/art-card.component.ts
+++ src/app/art-card/art-card.component.ts
@@ -13,6 +13,25 @@ export class ArtCardComponent implements OnInit {
     this.invokeStripe();
   }
 
+  makePayment(amount: any) {
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51KCY9mE56BwLuZepj0kzE3n8BLfLCfKhcav75HeIIpISFKe2xl1XCgSatNvKXRIogrhT0WMbnBg7nakDynqLOFSU00QcLgdt28',
+      locale: 'auto',
+      token: function(stripeToken: any) {
+        console.log(stripeToken.card);
+        alert('Stripe token generated!');
+      },
+    });
+
+    paymentHandler.open({
+      name: 'Artwork Africa',
+      description: 'Just checking',
+      amount: amount * 100,
+    })
+
+
+  }
+
   invokeStripe() {
     if(!window.document.getElementById('stripe-script')) {
       const script = window.document.createElement('script');
